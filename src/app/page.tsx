e42a1b1c6b72d70bc19e34717460a4d4e4698eab
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-11-20 18:46:50 +0500

    background added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Start Now!</Button>
+      <h1>H1</h1>
     </main>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-brandColour font-extrabold tracking-[-0.14em] lg:text-5xl border-2 border-black shadow-btn",
+          "bg-brandColour font-extrabold tracking-[-0.14em] border-2 border-black shadow-btn",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
